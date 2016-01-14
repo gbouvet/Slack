b8a6066c4b3cd1b7d16f0d6eb1ac2ab84b26a468
gbouvet <bouvet.gilles@gmail.com>
2016-01-15 00:55:32 +0100

    Minor modifications
    Test if replace is revelant.

--- capelobot.js
+++ capelobot.js
@@ -1,17 +1,20 @@
 module.exports = function (req, res, next) {
+	var userName = req.body.user_name;
 	var text = req.body.text;
 	
 	text = text.toLowerCase();
-
 	var pattern = /o(d|t)?|eau(d|x)?|o(d|t)?|au(d|t|lt|x)?/g;
-	text = text.toString().replace(pattern, "al");
+	var isPertinent = 0;
+	if(text.match(pattern) != null){
+		text = text.toString().replace(pattern, "al");
+		var botPayload = {
+			text : 'Si je peux me permettre...: ' + text + " :_capelo:"
+		};
+		isPertinent = 1;
+	}
 	
-	var botPayload = {
-		text : 'Si je peux me permettre...: ' + text + " 	:_capelo:"
-	};
-
 	// avoid infinite loop
-	if (userName !== 'slackbot') {
+	if (userName !== 'slackbot' && isPertinent == 1) {
 		return res.status(200).json(botPayload);
 	} else {
 		return res.status(200).end();
