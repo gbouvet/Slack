2ed64c325158ab076dad699a0c562646d73635a6
gbouvet <bouvet.gilles@gmail.com>
2016-01-23 22:22:30 +0100

    Minor fixes
    Fix for endings with "o"

--- capelobot.js
+++ capelobot.js
@@ -1,8 +1,9 @@
 module.exports = function (req, res, next) {
 	var userName = req.body.user_name;
 	var text = req.body.text;
-	var pattern = /o[^imnu]|eau|au/g;
-	var patternO = /o[^imnu]/;
+	var pattern = /o|o[^imnu]|eau|au/;
+	var patternO = /o/;
+	var patternOX = /o[^imnu]/;
 	var patternAu = /au/;
 	var patternEau = /eau/;
 	var patternFin = /[a-zA-Z]+[\.\?\!]/g;
@@ -15,15 +16,24 @@ module.exports = function (req, res, next) {
 		processedWord = words[i];
 		var copyW = new String(words[i].toString());
 		words[i] = words[i].toLowerCase();
-		while ((match = pattern.exec(words[i])) != null) {
+		while ((match = patternO.exec(words[i])) != null) {
 			if (words[i].length >= 3 && match.index > 0 ) {
 				words[i] = words[i].toString().replace(pattern, "al");
-				if (patternO.test(copyW)) {
+				if (patternOX.test(copyW)) {
 					var lastIndxO = copyW.toString().lastIndexOf('o');
-					if(lastIndxO == copyW.length - 1 || lastIndxO == copyW.length - 2 ) {
+					if(lastIndxO == copyW.length - 2 ) {
 						var firstPart = copyW.toString().substring(0,lastIndxO);
 						var secondPart = copyW.toString().substring(lastIndxO,copyW.length);
-						console.log('Debug o secondPart : ' + secondPart);
+						processedWord = firstPart + secondPart.replace(patternOX, "al");
+						if (isPertinent == 0) {
+							isPertinent = 1;
+						}
+					}
+				} else if (patternO.test(copyW)) {
+					var lastIndxO = copyW.toString().lastIndexOf('o');
+					if(lastIndxO == copyW.length - 1) {
+						var firstPart = copyW.toString().substring(0,lastIndxO);
+						var secondPart = copyW.toString().substring(lastIndxO,copyW.length);
 						processedWord = firstPart + secondPart.replace(patternO, "al");
 						if (isPertinent == 0) {
 							isPertinent = 1;
@@ -34,6 +44,7 @@ module.exports = function (req, res, next) {
 					if(lastIndxEau == copyW.length - 4 || lastIndxEau == copyW.length - 3 ) {
 						var firstPart = copyW.toString().substring(0,lastIndxEau);
 						var secondPart = copyW.toString().substring(lastIndxEau,copyW.length);
+						secondPart = secondPart.substring(0,secondPart.lastIndexOf('eau')+3);
 						processedWord = firstPart + secondPart.replace(patternEau, "al");
 						if (isPertinent == 0) {
 							isPertinent = 1;
@@ -44,7 +55,7 @@ module.exports = function (req, res, next) {
 					if(lastIndxAu == copyW.length - 2 || lastIndxAu == copyW.length - 3 ) {
 						var firstPart = copyW.toString().substring(0,lastIndxAu);
 						var secondPart = copyW.toString().substring(lastIndxAu,copyW.length);
-						console.log('Debug au secondPart : ' + secondPart);
+						secondPart = secondPart.substring(0,secondPart.lastIndexOf('au')+2);
 						processedWord = firstPart + secondPart.replace(patternAu, "al");
 						if (isPertinent == 0) {
 							isPertinent = 1;
